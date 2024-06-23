bfa27d24f3309e3b4ec0cff8b6bff2d927021d97
Ian Jamieson <i@njamieson.co.uk>
2024-06-23 12:43:45 +0100

    more stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  nitro: {
+    preset: 'cloudflare-pages'
+  },
   modules: [
     "@nuxt/content",
     "@nuxthq/studio",
     "@pinia/nuxt",
-    "@pinia-plugin-persistedstate/nuxt"
+    "@pinia-plugin-persistedstate/nuxt",
+    "@nuxt/image"
   ],
   css: ["~/assets/css/main.css"],
 })
